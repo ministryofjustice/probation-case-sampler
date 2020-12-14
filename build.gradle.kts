5f3cb7d7e7f6123d29882cdd087ee1a5ed58b4d7
Peter Phillips <peter@greenthistle.co.uk>
2020-12-14 15:57:36 +0000

    DT-1398: ⬆️ Upgrade to latest gradle plugin / spring boot etc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.6"
-    kotlin("plugin.spring") version "1.4.10"
-    kotlin("plugin.jpa") version "1.4.10"
+    id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+    kotlin("plugin.spring") version "1.4.21"
+    kotlin("plugin.jpa") version "1.4.21"
 }
 
 group = "uk.gov.justice.digital.hmiprobation"
@@ -21,9 +21,9 @@ dependencies {
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("net.sf.ehcache:ehcache:2.10.6")
     implementation("org.apache.commons:commons-lang3:3.11")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.11.2")
-    implementation( "com.fasterxml.jackson.module:jackson-module-kotlin:2.11.2")
-    implementation("com.google.guava:guava:29.0-jre")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.0")
+    implementation( "com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("com.google.guava:guava:30.0-jre")
 
 
     testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -35,7 +35,7 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
     testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
-    testImplementation("com.ninja-squad:springmockk:2.0.3")
+    testImplementation("com.ninja-squad:springmockk:3.0.0")
     testImplementation("io.jsonwebtoken:jjwt:0.9.1")
     testImplementation("org.apache.poi:poi:4.1.2")
     testImplementation("org.apache.poi:poi-ooxml:4.1.2")
